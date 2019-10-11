c07672eb0be7ce5771d45433bd7c336f56746f1a
Vishvanath Surwshe <v2018y@gmail.com>
2019-10-11 09:26:05 +0530

    0.1.13: Food Successfully CRUD Opertions according to  npm lib

--- src/components/secure/food/Food.js
+++ src/components/secure/food/Food.js
@@ -2,51 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import FormUI from 'vany-crud-modal';
 import * as actionsCre from "../../../action/index";
-
-
 class Food extends React.Component {
-
-
-    getFoodData = (token, data) => {
-        console.log("Get Data ", data)
-        if (token) {
-            this.props.loadFood(this.props.token.token)
-        }
+     // This Method Handel Get Actions for Executing
+    getFoodData = (token) => {
+        token &&  this.props.loadFood(token)
     }
-
     // This Method Handel Post Actions for Executing
     handelPostSubmit = (token, data) => {
-        console.log("Post Data ", data)
-        // if (errors.length === 0) {
-        //     this.props.saveFoodData(this.props.token.token, values)
-        // }
+        data &&  this.props.saveFoodData(token, data)
     }
     // This Method Handel Put Actions for Executing
     handelPutSubmit = (token, data) => {
-        console.log("Put Data ", data)
-        // if (errors.length === 0) {
-        //     this.setState({updateModel: !this.state.updateModel})
-        //     console.log("data ",values)
-        //     this.props.updateFoodData(this.props.token.token, values)
-        // }
+        data &&  this.props.updateFoodData(token, data)
     }
     // This Method Handel Delete Actions for Executing
     handelDeleteSubmit = (token, data) => {
-        console.log("Delete Data ", data)
-        // if (data.length === 0) {
-        //     this.setState({deleteModel: !this.state.deleteModel})
-        //     this.props.deleteFoodData(this.props.token.token, values)
-        // }
+        data &&  this.props.deleteFoodData(token, data)
     }
 
     render() {
-
         const filed = [
             { apiKey: 'foName', label: "Food Name", type: "text", required: true, errorMessage: "Enter Food Name", placeholder: "Ex: Pav Bahji" },
             { apiKey: 'foRate', label: "Food Price", type: "text", required: true, errorMessage: "Enter Food Price", placeholder: "Ex: jony" },
         ]
-
-
         return <FormUI
             token={this.props.token.token}
             getMethod={this.getFoodData}
@@ -59,10 +37,9 @@ class Food extends React.Component {
             primaryKey="foId"
         />
     }
-
 }
 
-const mapToPorps = (state) => {
+const mapStateToPorps = (state) => {
     return state;
 };
-export default connect(mapToPorps, actionsCre)(Food);
+export default connect(mapStateToPorps, actionsCre)(Food);
