f14d949121720d9a5e57cfbee9e449cdcec3a3d8
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-15 12:33:14 +0530

    0.3.1: Added SMS Form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { SMSForm } from './secure/SMSForm'
+import { Card, CardBody } from 'reactstrap'
 class Main extends React.Component {
-    
-    render() { 
-        return <h1> Hi FG</h1>
+
+    dataSubmit = (event, errors, values) => {
+        console.log(errors, values)
+    }
+
+    render() {
+        return <>
+            <Card>
+                <CardBody>
+                    <SMSForm dataSubmit={this.dataSubmit} />
+                </CardBody>
+            </Card>
+        </>
     }
 }
- 
+
 export default Main;
---
+++ src/components/secure/SMSForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, FormGroup, Label, Col } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
+
+export const SMSForm = (props) => <AvForm onSubmit={props.dataSubmit}>
+    <FormGroup row>
+        <Label sm={2}>Enter Email</Label>
+        <Col sm={10}>
+            <AvField type="email" name="email" id="exampleEmail" placeholder="Enter Your Email" />
+        </Col>
+    </FormGroup>
+    <FormGroup row>
+        <Label sm={2}>Enter Your Name</Label>
+        <Col sm={10}>
+            <AvField type="text" name="name" id="examplePassword" placeholder="Enter Your Name" />
+        </Col>
+    </FormGroup>
+    <FormGroup row>
+        <Label sm={2}>Select Date</Label>
+        <Col sm={10}>
+            <AvField type="date" name="date" placeholder="Enter Your Name" />
+        </Col>
+    </FormGroup>
+    <FormGroup row>
+        <Label sm={2}>Select Tabel</Label>
+        <Col sm={10}>
+            <AvField type="select" name="select" helpMessage="Idk, this is an example. Deal with it!">
+                <option>Tabel 1</option>
+                <option>Tabel 2</option>
+                <option>Tabel 3</option>
+                <option>Tabel 4</option>
+                <option>Tabel 5</option>
+            </AvField>
+        </Col>
+    </FormGroup>
+    <FormGroup check row>
+        <Col sm={{ size: 10, offset: 2 }}>
+            <Button>Data Submit</Button>
+        </Col>
+    </FormGroup>
+</AvForm>
